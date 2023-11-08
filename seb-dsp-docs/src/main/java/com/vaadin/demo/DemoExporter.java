ba18548f55647e6893855d0c68435a0b68726aa9
Sebastian Kühnau <seb@vaadin.com>
2023-11-08 11:30:40 +0100

    update file structures

--- seb-dsp-docs/src/main/java/com/vaadin/demo/DemoExporter.java
+++ seb-dsp-docs/src/main/java/com/vaadin/demo/DemoExporter.java
@@ -6,7 +6,7 @@ import com.vaadin.flow.component.webcomponent.WebComponent;
 import com.vaadin.flow.internal.ReflectTools;
 import com.vaadin.flow.theme.Theme;
 
-@Theme("seb-theme")
+@Theme("docs")
 public abstract class DemoExporter<T extends Component>
         extends WebComponentExporter<T> {
 
